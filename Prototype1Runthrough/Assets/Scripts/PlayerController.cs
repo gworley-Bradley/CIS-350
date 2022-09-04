628f5fb0dcab1789dab1218873c6c8681ba100aa
gworley-Bradley <gworley@mail.bradley.edu>
2022-09-04 01:09:31 -0500

    Added Prototype 1 Runthrough

---
+++ Prototype1Runthrough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+
+    public float forwardInput;
+    public float horizontalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move Forward with forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
---
+++ Prototype1Runthrough/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// attach this to player
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    //set this reference in the inspector
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerZone"))
+        {
+            ScoreManager.score++;
+
+        }
+    }
+}
