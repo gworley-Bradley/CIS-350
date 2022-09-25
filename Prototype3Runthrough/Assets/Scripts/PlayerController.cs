ad98adf6728fe10874c2a2096bfc0658330d3be1
gworley-Bradley <gworley@mail.bradley.edu>
2022-09-25 00:18:16 -0500

    Added Prototype3Runthrough

---
+++ Prototype3Runthrough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float jumpForce;
+    public ForceMode forceMode;
+    public float gravityModifier;
+
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    public Animator playerAnimator;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private AudioSource playerAudio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set reference to our rigidbody component
+        rb = GetComponent<Rigidbody>();
+        //set a reference to our animator component
+        playerAnimator = GetComponent<Animator>();
+
+        //start running animation on start
+        playerAnimator.SetFloat("Speed_f", 1.0f);
+
+        playerAudio = GetComponent<AudioSource>();
+
+        forceMode = ForceMode.Impulse;
+        if (Physics.gravity.y > -10)
+        {
+            Physics.gravity *= gravityModifier;
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+
+            rb.AddForce(Vector3.up * jumpForce, forceMode);
+            isOnGround = false;
+
+            //set the trigger to play the jump animation
+            playerAnimator.SetTrigger("Jump_trig");
+
+            //stop playing dirt particle on jump
+            dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+
+            // play dirt particles
+            dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+
+            dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+
+            //play death animation
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            //play explosion particle
+            explosionParticle.Play();
+        }
+    }
+}
---
+++ Prototype3Runthrough/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //save the starting position of the background to a Vector3 variable
+        startPosition = transform.position;
+        //set the repeatWidth to half of the width of the background using BoxCollider
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the background is further to the left than the repeatWidth, reset it to its starting position
+        if(transform.position.x < startPosition.x - repeatWidth)
+        {
+            transform.position = startPosition;
+        }
+        
+    }
+}
